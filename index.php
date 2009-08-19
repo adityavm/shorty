0f9684775f720f2ea195a82f38d88381a84ee835
Aditya Mukherjee <ditya@adityamukherjee.com>
2009-08-19 22:53:01 +0530

    Fixed Readme formatting, index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?
 if(!isset($_GET['short']) || $_GET['short'] == "")
-	die("<div style=\"color:#444;width:300px;margin:20% auto 0;text-align:center;font:20pt Myriad Pro,Lucida Grande,Lucida Sans Unicde,sans-serif\">You clearly don't belong.<br/>So why are you still here?<br/><br/><a href=\"http://adityamukherjee.com\" style=\"font-size:12pt;color:#888;text-decoration:none;border-bottom:1px solid #DDD\">You should be here.</a></div>");
+	die();
 
 $u = "DB_USERNAME";
 $p = "DB_PASSWORD";
